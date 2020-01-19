48fbc26f4d6d051ca32a686452d356ff6fd347d3
Tomas Moore <tomasmoore@outlook.com>
2020-01-19 22:07:57 +0000

    Added user defaults

---
+++ Dysgu/ViewControllerScripts/Challenge/ChallengeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChallengeViewController.swift
+//  Dysgu
+//
+//  Created by Tomas Moore on 14/01/2020.
+//  Copyright © 2020 Tomas Moore. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeViewController: UIViewController {
+
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Profile", style: .plain, target: self, action: #selector(profileSegue))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .plain, target: self, action: #selector(settingsSegue))
+        print(defaults.string(forKey: "Username"))
+    }
+    
+    @objc func profileSegue(){
+        self.performSegue(withIdentifier: "profileSegue", sender: self)
+    }
+            
+    @objc func settingsSegue(){
+        self.performSegue(withIdentifier: "settingsSegue", sender: self)
+    }
+}
